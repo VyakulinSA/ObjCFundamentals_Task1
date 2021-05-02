282001266ea350822b9d5eaa43d5f1da01908baf
VyakulinSA <VyakulinSA@gmail.com>
2021-05-02 14:08:29 +0400

    complete task 1_1

--- ObjCFundamentals_Task1/main.m
+++ ObjCFundamentals_Task1/main.m
@@ -6,11 +6,25 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Circle.h"
+#import "Rectangle.h"
+#import "Triangle.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        Circle *circle = [Circle createCircle];
+        circle.radius = 4;
+        NSLog(@"Description: %@", [circle description]);
+        
+        Rectangle *rectangle = [[Rectangle alloc] initWithSides_width:2.4335 height:5.432];
+        NSLog(@"Description: %@", [rectangle description]);
+        
+        Triangle *triangle = [Triangle createTriangleWithSides_aSide:3 bSide:4 cSide:5];
+        NSLog(@"Description: %@", [triangle description]);
+        
+
+    
     }
     return 0;
 }
