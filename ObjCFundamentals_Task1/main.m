58eb2c7e74f845a07efd0afd54a1b221c1c87014
VyakulinSA <VyakulinSA@gmail.com>
2021-05-02 11:06:45 +0400

    Initial Commit

---
+++ ObjCFundamentals_Task1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjCFundamentals_Task1
+//
+//  Created by Вякулин Сергей on 02.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
